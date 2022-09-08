f084c16911fd7b656b4585b29a584d2b56fdbe1e
Sam <klioop2@gmail.com>
2022-09-08 22:24:29 +0900

    Move production code to the production scope

---
+++ KlioopUnsplash/Shared API/RemoteLoader.swift
@@ -0,0 +1,57 @@
+//
+//  RemoteLoader.swift
+//  KlioopUnsplash
+//
+//  Created by klioop on 2022/09/08.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
+    func perform(_ request: URLRequest, completion: @escaping (Result) -> Void)
+}
+
+public final class RemoteLoader<Resource> {
+    public typealias Mapper = (Data, HTTPURLResponse) throws -> Resource
+    
+    private let request: URLRequest
+    private let client: HTTPClient
+    private let mapper: Mapper
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public init(request: URLRequest, client: HTTPClient, mapper: @escaping Mapper) {
+        self.request = request
+        self.client = client
+        self.mapper = mapper
+    }
+    
+    public func load(completion: @escaping (Result<Resource, Swift.Error>) -> Void) {
+        client.perform(request) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case let .success((data, response)):
+                completion(self.map(data, from: response))
+                
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+    
+    private func map(_ data: Data, from response: HTTPURLResponse) -> Result<Resource, Swift.Error> {
+        Result {
+            do {
+                return try mapper(data, response)
+            } catch {
+                throw Error.invalidData
+            }
+        }
+    }
+}
--- KlioopUnsplashTests/Shared API/RemoteLoaderTests.swift
+++ KlioopUnsplashTests/Shared API/RemoteLoaderTests.swift
@@ -6,55 +6,7 @@
 //
 
 import XCTest
-
-protocol HTTPClient {
-    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
-    
-    func perform(_ request: URLRequest, completion: @escaping (Result) -> Void)
-}
-
-final class RemoteLoader<Resource> {
-    typealias Mapper = (Data, HTTPURLResponse) throws -> Resource
-    
-    private let request: URLRequest
-    private let client: HTTPClient
-    private let mapper: Mapper
-    
-    enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    init(request: URLRequest, client: HTTPClient, mapper: @escaping Mapper) {
-        self.request = request
-        self.client = client
-        self.mapper = mapper
-    }
-    
-    func load(completion: @escaping (Result<Resource, Swift.Error>) -> Void) {
-        client.perform(request) { [weak self] result in
-            guard let self = self else { return }
-            
-            switch result {
-            case let .success((data, response)):
-                completion(self.map(data, from: response))
-                
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        }
-    }
-    
-    private func map(_ data: Data, from response: HTTPURLResponse) -> Result<Resource, Swift.Error> {
-        Result {
-            do {
-                return try mapper(data, response)
-            } catch {
-                throw Error.invalidData
-            }
-        }
-    }
-}
+import KlioopUnsplash
 
 class RemoteLoaderTests: XCTestCase {
     
