1a19c6779dfd31905a4194190dc0ee5511ae8588
Sam <klioop2@gmail.com>
2022-09-08 21:53:21 +0900

    Generic RemoteLoader.load delivers resource on success

--- KlioopUnsplashTests/Shared API/RemoteLoaderTests.swift
+++ KlioopUnsplashTests/Shared API/RemoteLoaderTests.swift
@@ -8,25 +8,38 @@
 import XCTest
 
 protocol HTTPClient {
-    typealias Result = Swift.Result<Void, Error>
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
     
     func perform(_ request: URLRequest, completion: @escaping (Result) -> Void)
 }
 
-final class RemoteLoader {
+final class RemoteLoader<Resource> {
+    typealias Mapper = (Data, HTTPURLResponse) -> Resource
+    
     private let request: URLRequest
     private let client: HTTPClient
+    private let mapper: Mapper
     
-    init(request: URLRequest, client: HTTPClient) {
-        self.request = request
-        self.client = client
+    enum Error: Swift.Error {
+        case connectivity
     }
     
-    func load(completion: @escaping (Result<Void, Error>) -> Void) {
-        client.perform(request) { result in
-            if case let .failure(error) = result {
-                completion(.failure(error))
+    init(request: URLRequest, client: HTTPClient, mapper: @escaping Mapper) {
+        self.request = request
+        self.client = client
+        self.mapper = mapper
+    }
+    
+    func load(completion: @escaping (Result<Resource, Error>) -> Void) {
+        client.perform(request) { [weak self] result in
+            switch result {
+            case let .success((data, response)):
+                completion(.success(self!.mapper(data, response)))
+                
+            case .failure:
+                completion(.failure(Error.connectivity))
             }
+            
         }
     }
 }
@@ -61,14 +74,36 @@ class RemoteLoaderTests: XCTestCase {
                         
         client.completeLoading(with: error)
         
-        XCTAssertEqual(receivedError as? NSError, error)
+        XCTAssertNotNil(receivedError)
     }
     
+    func test_load_deliversResourceOnSuccess() {
+        let resource = "a resource"
+        let (sut, client) = makeSUT(mapper: { _, _ in
+            "a resource"
+        })
+
+        var receivedResource: String?
+        sut.load() {
+            receivedResource = try? $0.get()
+        }
+
+        client.completeLoadingSuccessfully(with: response())
+
+        XCTAssertEqual(receivedResource, resource)
+    }
+
     // MARK: - Helpers
     
-    private func makeSUT(request: URLRequest = URLRequest(url: anyURL()), file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteLoader, client: ClientSpy) {
+    typealias SUT = RemoteLoader<String>
+    
+    private func makeSUT(
+        request: URLRequest = URLRequest(url: anyURL()),
+        mapper: @escaping SUT.Mapper = { _, _ in "any" },
+        file: StaticString = #filePath,
+        line: UInt = #line) -> (sut: SUT, client: ClientSpy) {
         let client = ClientSpy()
-        let sut = RemoteLoader(request: request, client: client)
+        let sut = RemoteLoader(request: request, client: client, mapper: mapper)
         trackMemoryLeak(client)
         trackMemoryLeak(sut)
         return (sut, client)
@@ -90,9 +125,21 @@ class RemoteLoaderTests: XCTestCase {
         func completeLoading(at index: Int = 0, with error: Error) {
             completions[index](.failure(error))
         }
+        
+        func completeLoadingSuccessfully(at index: Int = 0, with response: (Data, HTTPURLResponse)) {
+            completions[index](.success(response))
+        }
     }
     
     private func anyRequest(url: URL = anyURL()) -> URLRequest {
         URLRequest(url: url)
+    }
+    
+    private func response(with data: Data = Data("any".utf8)) -> (data: Data, response: HTTPURLResponse) {
+        (data, anyHTTPURLResponse())
+    }
+    
+    private func anyHTTPURLResponse(with code: Int = 200) -> HTTPURLResponse {
+        HTTPURLResponse(url: anyURL(), statusCode: code, httpVersion: nil, headerFields: nil)!
     }
 }
